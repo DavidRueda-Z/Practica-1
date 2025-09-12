dab59b3ef9c4d761735f574784f910e03dedc363
cjulianmd <julian.gallo.v@gmail.com>
2025-09-11 20:04:34 -0500

    comit corrijiendo errrores y finalizandolo

--- ejercicio 1/EspacioCarro.cs
+++ ejercicio 1/EspacioCarro.cs
@@ -1,11 +1,9 @@
-ï»¿using System;
+using System;
 
 namespace ejercicio_1
 {
     public class EspacioCarro : EspacioParqueo
     {
-        private Ticket ticketActual;
-
         public EspacioCarro(int id, decimal tarifaHora)
         {
             Id = id;
@@ -30,36 +28,30 @@ namespace ejercicio_1
             };
 
             EstaOcupado = true;
-
             Console.WriteLine($"Vehículo {placaVehiculo} ocupó el espacio {Id} a las {horaEntrada}");
         }
 
-        public override decimal Liberar(DateTime horaSalida)
+        public override Ticket Liberar(DateTime horaSalida)
         {
             if (!EstaOcupado)
             {
                 Console.WriteLine($"El espacio {Id} ya está libre.");
-
-                return 0;
+                return null;
             }
 
             ticketActual.HoraSalida = horaSalida;
-
             TimeSpan duracion = ticketActual.HoraSalida - ticketActual.HoraEntrada;
 
-            decimal total = ticketActual.CalcularCobro(duracion, TarifaHora, false); // esElectrico: false por defecto
-
+            ticketActual.CalcularCobro(duracion, TarifaHora, false);
             EstaOcupado = false;
 
-            Console.WriteLine($"Vehículo {ticketActual.Placa} liberó el espacio {Id} a las {horaSalida}. Total a pagar: {total:C}");
-
-            return total;
+            Console.WriteLine($"Vehículo {ticketActual.Placa} liberó el espacio {Id} a las {horaSalida}. Total a pagar: {ticketActual.ValorCobrado:C}");
+            return ticketActual;
         }
 
         public override void MostrarInformacion()
         {
             Console.WriteLine($"Espacio {Id} - Tipo: {Tipo} - Tarifa: {TarifaHora:C} - Ocupado: {EstaOcupado}");
-
             if (EstaOcupado && ticketActual != null)
             {
                 ticketActual.MostrarTicket();
